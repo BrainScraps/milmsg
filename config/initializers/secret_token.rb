8eaaf6eb041256fbb16bfb908ba23821c012a9fd
Isaac Elias <nicecurelom@gmail.com>
2012-11-10 12:50:49 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Milmsg::Application.config.secret_token = '99cf74ae16b495b59b7c027974203220343cd3e2534f03ea40fc38776f6f6b7fb297cea080ab1c01fd6a2bf4ce2fdfae7898c400a1c91042c780535b0900a3fc'
