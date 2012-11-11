9cb5716f3c9c1fd66057fe01a4f8ab380b30ed55
Isaac Elias <nicecurelom@gmail.com>
2012-11-11 11:52:59 -0800

    migration gone

--- db/migrate/20121111100727_remove_password_hash_from_users.rb
+++ db/migrate/20121111100727_remove_password_hash_from_users.rb
@@ -1,11 +1,11 @@
 class RemovePasswordHashFromUsers < ActiveRecord::Migration
   def up
-    remove_column :users, :password_hash
+    remove_column :users, :pass_hash
     rename_column :users, :password_digest, :password
   end
  
   def down
-    add_column :users, :password_hash, :string
+    add_column :users, :pass_hash, :string
     rename_column :users, :password, :password_digest
   end
 end
--- db/migrate/20121111101358_remove_password_hash_from_userss.rb
+++
@@ -1,9 +0,0 @@
-class RemovePasswordHashFromUserss < ActiveRecord::Migration
-  def up
-  	remove_column :users, :pass_hash
-  end
-
-  def down
-  	add_column :users, :pass_hash, :string
-  end
-end
